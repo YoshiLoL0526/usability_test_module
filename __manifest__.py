366ba724d4b99a18226a56a04775e6fc66b21315
Raidel Martínez Santos <121899025+YoshiLoL0526@users.noreply.github.com>
2024-08-30 12:37:29 -0400

    Add files via upload

--- __manifest__.py
+++ __manifest__.py
@@ -1,15 +1,20 @@
 {
     'name': 'Usability Testing Module',
-    'version': '17.0.1.0',
+    'version': '17.0.1.0.0',
     'category': 'Tools',
     'summary': 'Module for managing usability testing in software development',
-    'author': 'R&R',
+    'author': 'Raidel Martínez Santos & Robert Hillary Serna',
     'website': 'https://github.com/YoshiLoL0526/usability_test_module',
     'depends': ['base'],
     'data': [
         'views/base_views.xml',
         'views/tests_views.xml',
         'views/principles_views.xml',
+        'security/ir.model.access.csv',
+        'data/principios.xml'
+    ],
+    'demo': [
+        'demo/demo.xml',
     ],
     'installable': True,
     'auto_install': False,
--- models/usability_principles.py
+++ models/usability_principles.py
@@ -9,6 +9,14 @@ class UsabilityPrinciples(models.Model):
     name = fields.Char('Nombre', required=True)
     description = fields.Text('Descripción')
 
+    @api.constrains('description')
+    def _check_weird_chars(self):
+        for record in self:
+            for char in record.description:
+                if 33 <= ord(char) <= 47 or 58 <= ord(char) <= 64 or 91 <= ord(char) <= 96 or 123 <= ord(char) <= 126:
+                # if char in '_+=-_/?{}[]()*&^%$#@!~`':
+                    raise ValidationError("La descripción no puede contener caracteres extraños.")
+
     @api.constrains('name')
     def _check_name_length(self):
         for record in self:
